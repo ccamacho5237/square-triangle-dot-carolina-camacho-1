c34670217a981bcfd3f7db104e72cb4362131717
CAROLINA MARÍA CAMACHO ZAMBRANO <36022220+ccamacho5237@users.noreply.github.com>
2021-05-01 19:14:47 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,100 @@
+basic.showIcon(IconNames.Square)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # # #
+    . . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . # #
+    . . . . #
+    . . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . . . #
+    . # . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . # . #
+    . # . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . . . #
+    . # . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . #
+    . . . . #
+    . # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . . #
+    . . # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
